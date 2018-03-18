58c5c8ac94fb8ce57e3d0258ad5c4349ebbed099
Marvin Kastner <1kastner@users.noreply.github.com>
2018-03-18 22:43:07 +0100

    Make both players be a bot with random actions, improve interface

--- pyvolley/aiinterface.py
+++ pyvolley/aiinterface.py
@@ -4,28 +4,73 @@ from cocos.layer import Layer
 
 from . import constants
 
+__author__ = '1kastner'
+
 class AIInterface(Layer):
     def __init__(self, game):
         super(AIInterface, self).__init__()
-        self.ball = game.ball.body
-        self.player1 = game.players[0]
-        self.player2 = game.players[1]
-        self.input_key_press = game.on_key_press
-        self.input_key_release = game.on_key_release
-        self.player2config = game.config_player[1]
+        self.bot_1 = Bot(
+            game.ball.body,
+            Controller(
+                lambda : game.on_key_press(game.config_player[0]['left'], None),
+                lambda : game.on_key_press(game.config_player[0]['right'], None),
+                lambda : game.on_key_press(game.config_player[0]['jump'], None),
+                lambda : game.on_key_release(game.config_player[0]['left'], None),
+                lambda : game.on_key_release(game.config_player[0]['right'], None),
+                lambda : game.on_key_release(game.config_player[0]['jump'], None)                   
+            )
+        )
+        self.bot_2 = Bot(
+            game.ball.body,
+            Controller(
+                lambda : game.on_key_press(game.config_player[1]['left'], None),
+                lambda : game.on_key_press(game.config_player[1]['right'], None),
+                lambda : game.on_key_press(game.config_player[1]['jump'], None),
+                lambda : game.on_key_release(game.config_player[1]['left'], None),
+                lambda : game.on_key_release(game.config_player[1]['right'], None),
+                lambda : game.on_key_release(game.config_player[1]['jump'], None)              
+            )
+        )
+        self.game = game
+        
         self.schedule_interval(self.update, 1./constants.FPS)
-        self.key_pressed = {'left': False, 'right': False, 'jump': False}
-    
+
     def update(self, dt):
+        pos_1 = self.game.players[0].body.position
+        pos_2 = self.game.players[1].body.position
+        self.bot_1.update(pos_1, pos_2)
+        self.bot_2.update(pos_2, pos_1)
         #print("P1", self.player1.body.position)
         #print("P2", self.player2.body.position)
         #print("ball", self.ball.position)
-        action = random.choice(['left', 'right', 'jump'])
+
+
+
+class Controller:
+    def __init__(self, left_start, right_start, jump_start, left_end, right_end, jump_end):
+        self.left_start = lambda : left_start() or "left"
+        self.right_start = lambda : right_start() or "right"
+        self.jump_start = lambda : jump_start() or "jump"
+        self.left_end = lambda : left_end() or "left"
+        self.right_end = lambda : right_end() or "right"
+        self.jump_end = lambda : jump_end() or "jump"
+
+class Bot:
+    def __init__(self, ball, controller):
+        self.ball = ball
+        self.controller = controller
+        self.key_pressed = {'left':False, 'right':False, 'jump':False}
+        
+    def update(self, pos_me, pos_other):
+        print(self.ball.position)
+        print(pos_me)
+        print(pos_other)
+        action = random.choice([
+            self.controller.left_start if self.key_pressed['left'] else self.controller.left_end,
+            self.controller.right_start if self.key_pressed['right'] else self.controller.right_end,
+            self.controller.jump_start if self.key_pressed['jump'] else self.controller.jump_end,
+        ])
         if random.random() < 0.1:
-            if self.key_pressed[action]:
-                print("start action", action)
-                self.input_key_press(self.player2config[action], None)
-            else:
-                print("stop action", action)
-                self.input_key_release(self.player2config[action], None)
-            self.key_pressed[action] = not self.key_pressed[action]
+            action_name = action()
+            self.key_pressed[action_name] = not self.key_pressed[action_name]
+            print(action_name, self.key_pressed)
